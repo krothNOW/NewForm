fae8a8df1463fdcdff45d04ec731dfe0033d0579
krothNOW <kroth@nowcourier.com>
2018-06-08 19:37:03 -0400

    II

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 var gulp = require("gulp");
 
-gulp.task("hello", function() {
-  return new Promise(function(resolve, reject) {
-    console.log("Hello Zell");
-    resolve();
-  });
-});
-
-var browserSync = require("browser-sync").create();
-
-gulp.task("browserSync", function() {
-  browserSync.init({
-    server: {
-      baseDir: "NewForm"
-    }
-  });
+gulp.task("default", function() {
+  return(
+    console.log("Kevbot")
+  );
 });
